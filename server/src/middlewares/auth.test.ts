1b6f1ab50be9d618f2a14d6cef7a417677cc82ef
nwnp <jjw_0418@naver.com>
2023-05-01 17:46:29 +0900

    feat: auth middleware making

--- server/src/middlewares/auth.test.ts
+++ server/src/middlewares/auth.test.ts
@@ -1,7 +1,23 @@
 import { describe, it, expect } from "@jest/globals";
 import { auth } from "./auth";
-// import httpMocks from "node-mocks-http";
+// import { faker } from "@faker-js/faker";
+import httpMocks from "node-mocks-http";
+import { NextFunction } from "express";
 
 describe("", () => {
-  it("", () => {});
+  it("returns 404 when is not Header", async () => {
+    const req = httpMocks.createRequest({});
+    const res = httpMocks.createResponse();
+    const next: NextFunction = jest.fn();
+
+    await auth(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._getJSONData()).toStrictEqual({
+      result: "1",
+      message: "need to Bearer Token in Header",
+    });
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(next).not.toBeCalled();
+  });
 });
--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,3 +1,14 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 
-export const auth = (req: Request, res: Response) => {};
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(404).json({
+      result: "1",
+      message: "need to Bearer Token in Header",
+    });
+  }
+
+  next();
+};
