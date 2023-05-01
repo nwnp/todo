98b2147d2e33fac05c0d2c278b55beecb9667232
nwnp-desktop <jjw_0418@naver.com>
2023-05-01 21:34:24 +0900

    feat: index page - error handling

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,7 +8,12 @@ router.get("/", async (req: Request, res: Response) => {
   // TODO: client domain으로 redirect 시키기
   const userRepository = new UserRepository();
   const user: IUser = await userRepository.getUserById(1);
-  console.log(user.email);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Not Founded User"
+    })
+  }
 
   res.status(200).json(user);
 });
