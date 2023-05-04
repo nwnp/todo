b14e9aa218ce564a84846a0ef5f4a051287a916d
nwnp-desktop <jjw_0418@naver.com>
2023-05-05 02:59:28 +0900

    feat: auth middleware fix

--- server/src/middlewares/auth.test.ts
+++ server/src/middlewares/auth.test.ts
@@ -1,11 +1,12 @@
 import { describe, it, expect } from "@jest/globals";
 import { auth } from "./auth";
-// import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import httpMocks from "node-mocks-http";
 import { NextFunction } from "express";
 
 describe("", () => {
-  it("returns 404 when is not Header", async () => {
+  it("return 404 when token is not exist", async () => {
+    const token = faker.datatype.string();
     const req = httpMocks.createRequest({});
     const res = httpMocks.createResponse();
     const next: NextFunction = jest.fn();
@@ -14,8 +15,29 @@ describe("", () => {
 
     expect(res.statusCode).toBe(404);
     expect(res._getJSONData()).toStrictEqual({
-      result: "1",
-      message: "need to Bearer Token in Header",
+      status: 4,
+      message: "Unauthorization",
+    });
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(next).not.toBeCalled();
+  });
+
+  it("return 404 when not bearer token", async () => {
+    const token = faker.datatype.string();
+    const req = httpMocks.createRequest({
+      headers: {
+        authorization: token,
+      },
+    });
+    const res = httpMocks.createResponse();
+    const next: NextFunction = jest.fn();
+
+    await auth(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._getJSONData()).toStrictEqual({
+      status: 4,
+      message: "Unauthorization",
     });
     expect(next).toHaveBeenCalledTimes(0);
     expect(next).not.toBeCalled();
