06f3fd50d2189fb05da4830bdd1207868ec3c8ed
nwnp-desktop <jjw_0418@naver.com>
2023-05-05 02:49:36 +0900

    feat: copilot setup and auth middleware

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -3,10 +3,28 @@ import { Response, Request, NextFunction } from "express";
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
 
+  // return when token is not exist
   if (!token) {
     return res.status(404).json({
-      result: "1",
-      message: "need to Bearer Token in Header",
+      status: 4,
+      message: "Unauthorization",
+    });
+  }
+
+  // return when not bearer token
+  if (!token.startsWith("Bearer ")) {
+    return res.status(404).json({
+      status: 4,
+      message: "Unauthorization",
+    });
+  }
+
+  // return when token is not valid
+  const tokenValue = token.split(" ")[1];
+  if (tokenValue !== "1234") {
+    return res.status(404).json({
+      status: 4,
+      message: "Unauthorization",
     });
   }
 
