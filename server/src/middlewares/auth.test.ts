1a35939810498b41557b64720ffbedc342ad5ccc
nwnp <jjw_0418@naver.com>
2023-05-09 22:21:58 +0900

    feat: Auth middleware with test code

--- server/src/middlewares/auth.test.ts
+++ server/src/middlewares/auth.test.ts
@@ -1,10 +1,12 @@
+import jwt from "jsonwebtoken";
 import { describe, it, expect } from "@jest/globals";
 import { auth } from "./auth";
 // import { faker } from "@faker-js/faker";
 import httpMocks from "node-mocks-http";
 import { NextFunction } from "express";
+import { faker } from "@faker-js/faker";
 
-describe("", () => {
+describe("Auth Middleware", () => {
   it("returns 404 when is not Header", async () => {
     const req = httpMocks.createRequest({});
     const res = httpMocks.createResponse();
@@ -19,5 +21,67 @@ describe("", () => {
     });
     expect(next).toHaveBeenCalledTimes(0);
     expect(next).not.toBeCalled();
+  });
+
+  it("returns 404 when is not Bearer Token", async () => {
+    const req = httpMocks.createRequest({
+      headers: {
+        authorization: "invalidtoken",
+      },
+    });
+    const res = httpMocks.createResponse();
+    const next: NextFunction = jest.fn();
+
+    await auth(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._getJSONData()).toStrictEqual({
+      result: "1",
+      message: "need to Bearer Token in Header",
+    });
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 404 when is not valid Token", async () => {
+    const token = faker.random.alphaNumeric(20);
+    const req = httpMocks.createRequest({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    const res = httpMocks.createResponse();
+    const next: NextFunction = jest.fn();
+
+    await auth(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._getJSONData()).toStrictEqual({
+      result: "1",
+      message: "Authentication Error",
+    });
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 200 when is valid Token", async () => {
+    const token = jwt.sign(
+      {
+        id: "fakeid",
+        password: "fakepassword",
+        email: "fakeemail",
+      },
+      "jwtsecret"
+    );
+    const req = httpMocks.createRequest({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    const res = httpMocks.createResponse();
+    const next: NextFunction = jest.fn();
+
+    await auth(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(next).toBeCalled();
   });
 });
--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,32 +1,56 @@
+import dotenv from "dotenv";
 import { Response, Request, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const USER = {
+  id: "fakeid",
+  password: "fakepassword",
+  email: "fakeemail",
+};
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
 
-  // return when token is not exist
   if (!token) {
     return res.status(404).json({
-      status: 4,
-      message: "Unauthorization",
+      result: "1",
+      message: "need to Bearer Token in Header",
     });
   }
 
-  // return when not bearer token
   if (!token.startsWith("Bearer ")) {
     return res.status(404).json({
-      status: 4,
-      message: "Unauthorization",
+      result: "1",
+      message: "need to Bearer Token in Header",
     });
   }
 
-  // return when token is not valid
   const tokenValue = token.split(" ")[1];
-  if (tokenValue !== "1234") {
-    return res.status(404).json({
-      status: 4,
-      message: "Unauthorization",
-    });
-  }
+  jwt.verify(
+    tokenValue,
+    process.env.JWT_SECRET as string,
+    async (err, decoded) => {
+      if (err) {
+        return res.status(404).json({
+          result: "1",
+          message: "Authentication Error",
+        });
+      }
 
-  next();
+      const user = jwt.verify(
+        tokenValue,
+        process.env.JWT_SECRET as string
+      ) as typeof USER;
+      if (user.id !== USER.id) {
+        return res.status(404).json({
+          result: "1",
+          message: "Authentication Error",
+        });
+      }
+
+      next();
+    }
+  );
 };
